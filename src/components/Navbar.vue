e69e5823f31bd4cfbe19a2ea3132535c218dc4d2
unknown <thalita.sok@student.passerellesnumeriques.org>
2026-06-29 19:32:12 +0700

    changed:icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
       <router-link to="/" class="navbar-brand">
+        <img src="../assets/icon.png" alt="icon" class="icon">
         <strong>SpiceLeaf Market</strong>
       </router-link>
 
@@ -146,7 +147,9 @@ const logout = async () => {
   align-items: center;
   gap: 8px;
 }
-
+.icon{
+  width: 50px;
+}
 .nav-link {
   color: rgba(255, 255, 255, 0.84) !important;
   font-weight: 600;
